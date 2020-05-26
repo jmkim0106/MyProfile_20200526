1da56d8a9fb5389d8bf240661ccc28512707fc18
weekyeon <weekyeon@gmail.com>
2020-05-26 17:47:56 +0900

    Glide로 이미지 띄우기

--- app/src/main/java/com/example/myprofile_20200526/MainActivity.java
+++ app/src/main/java/com/example/myprofile_20200526/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.example.myprofile_20200526.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
@@ -26,6 +27,9 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+//        인터넷에 있는 이미지 불러오기 -> 인터넷 연결 권한 필요
+        Glide.with(mContext).load("https://pbs.twimg.com/media/EAK0c11U8AE44aU.jpg").into(binding.profileImg);
 
     }
 }
